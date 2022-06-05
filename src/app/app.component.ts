a41eab60564fab187e0cf29427db21fc5b5fe941
Rafael Cuchilla <antonio9550@gmail.com>
2022-06-05 16:45:37 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+ //template:'<span>Rafael</span>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  
+
+  
 }
